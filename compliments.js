3124329416069efdeaa504adc0765bdd2e60cb50
ItsAToasty <78262341+ItsAToasty@users.noreply.github.com>
2021-02-09 09:09:42 +0100

    Add files via upload

---
+++ compliments.js
@@ -0,0 +1,25 @@
+{
+    "anytime" : [
+		"Sieh es positiv,/n schlimmer als heute kannst du nicht mehr aussehen!",
+		"Fast so Geil wie @herr_chris_euginski !",
+		"Falls es dir heute noch niemand gesagt hat, /n Geiler Arsch!"
+    ],
+    "morning" : [
+        "Guten Morgen, Sonnenschein.",
+        "Erstmal nh Kaffee?",
+        "Schnapp sie dir, Tiger!"
+    ],
+    "afternoon" : [
+        "Hitting your stride!",
+        "You are making a difference!",
+        "You're more fun than bubble wrap!"
+    ],
+    "evening" : [
+        "You made someone smile today, I know it."
+        "You are making a difference.",
+        "The day was better for your efforts."
+    ],
+	"day_sunny" : [
+		"Geiles Wetter? Geiler Tag!"
+    ]
+}
